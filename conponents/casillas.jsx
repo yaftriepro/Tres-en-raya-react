ac4aa688be692ae980f122589768952e03f41e5d
Yaftriepro <88110755+yaftriepro@users.noreply.github.com>
2022-04-14 21:52:35 +0200

    Create casillas.jsx

---
+++ conponents/casillas.jsx
@@ -0,0 +1,10 @@
+import React from "react"
+import { Casilla } from "./casilla"
+
+export function Casillas() {
+    return (
+        <div>
+              <Casilla /> <Casilla /> <Casilla />
+        </div>
+    )
+}
